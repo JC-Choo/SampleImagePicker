792b37e89cacdbf91cb431ff69b8dfe1c86187fb
JC-Choo <jicheolchoo@gmail.com>
2021-01-18 22:23:20 +0900

    Add ImagePicker

--- app/src/main/java/dev/chu/imagepickersample/MainActivity.kt
+++ app/src/main/java/dev/chu/imagepickersample/MainActivity.kt
@@ -1,11 +1,24 @@
 package dev.chu.imagepickersample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imagePicker: ImagePicker
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imagePicker = ImagePicker(this) { uri ->
+            findViewById<ImageView>(R.id.imageView).setImageURI(uri)
+        }
+
+        findViewById<Button>(R.id.setImageButton).setOnClickListener {
+            imagePicker.show()
+        }
     }
 }
